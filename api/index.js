1f0a0cacba8d80a3f2e1f643da19e57093c325db
Diane <hipolitodianalei@gmail.com>
2024-12-03 12:13:48 +0800

    decoy8

--- api/index.js
+++ api/index.js
@@ -29,6 +29,58 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
 // let songId = songs.length;
 
 // http://localhost:4000/songs
+
+app.get('/', (request, response) => {
+    //response.status(200).json({ message: 'API Homepage' });
+    response.send(`<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Demo-07-NodeJS-Express-Routing</title>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+          <style>
+            body {
+              font-family: 'Roboto', sans-serif;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              height: 90vh;
+              background-color: #f4f4f4;
+            }
+  
+            .container {
+              text-align: center;
+              color: #333;
+            }
+  
+            h1 {
+              font-size: 3rem;
+              margin-bottom: 10px;
+              text-shadow: 0 0 0 transparent;
+              transition: text-shadow 0.5s ease-in-out;
+            }
+
+            h1:hover{
+              text-shadow: 5px 5px 20px gray;
+            }
+  
+            h4 {
+              font-size: 1.5rem;
+              font-weight: 400;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>PRIN144-Final-Exam</h1>
+            <h4>Hipolito, Diana Lei E.</h4>
+          </div>
+        </body>
+      </html>
+    `);
+});
+
 app.get('/songs', async (req, res) => {
     try {
         if (req.query && req.query.id) {
